d269603cf761a9ccc5123c3fb8680df079845763
Shaik-Mohammed-Ali <mdalishaik946@gmail.com>
2025-01-23 19:48:14 +0000

    Extra Comments Deleted

--- joke.js
+++ joke.js
@@ -1,29 +1,3 @@
-// document.getElementById('joke').addEventListener('click',getJoke);
-
-
-// async function getJoke(){
-//     const jokeElement = document.getElementById('joke');
-//     jokeElement.innerHTML = 'Loading..........'
-//     console.log("Loading");
-// }
-
-// try{
-//     const response = await fetch('https://official-joke-api.appspot.com/random_joke');
-
-//     if(!response.ok){
-//         throw new Error('Fetching the Data');
-//     }
-
-//     const jokeData = await response.json();
-//     jokeData.innerHTML = `${jokeData.setup} <br><strong>${jokeData.punchline}<strong>`
-// }catch (error){
-//     jokeElement.innerHTML='Oops! Something went Wrong with your Joke'
-// }
-
-
-// script.js
-//https://humor-jokes-and-memes.p.rapidapi.com/jokes/create?topics=pirates&max-length=1000
-
 document.getElementById('getJokeBtn').addEventListener('click', getJoke);
 
 // Function to fetch a random joke from the API
